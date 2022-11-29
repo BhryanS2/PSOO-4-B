a72f895444e2273925eb75178d7b077733b5c350
BhryanS2 <bhryanstepenhen@gmail.com>
2022-11-29 15:12:27 -0300

    deleting

--- php.php
+++ php.php
@@ -3,4 +3,19 @@ echo "<h1>PHP</h1>";
 
 require "./Backend/connection.php";
 
+?>
+<h1>connection</h1>
+<?php
 print_r($conn);
+$sql = "SELECT * FROM `users`";
+$statement = $conn->prepare($sql);
+$statement->execute();
+$users = $statement->fetchAll(PDO::FETCH_ASSOC);
+?>
+
+<h1>users</h1>
+<pre>
+	<?php
+	print_r($users);
+	?>
+</pre>

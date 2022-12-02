96220e4aaf4c04e2f6082e392686cf1bac84259e
BhryanS2 <bhryanstepenhen@gmail.com>
2022-12-02 09:58:18 -0300

    a

--- backend/services/questions/GetAllService.php
+++ backend/services/questions/GetAllService.php
@@ -62,6 +62,7 @@ class GetAllQuestionsService
 	{
 
 		$response = array();
+
 		$sql = "SELECT questions.id,
     questions.content,
     questions.lesson_id,
@@ -72,24 +73,15 @@ class GetAllQuestionsService
     alternatives.isCorrect,
     alternatives.id as alternative_id
     FROM questions INNER JOIN alternatives ON questions.id = alternatives.question_id";
-		$reponse = array();
-		$sql = "SELECT id, name, email FROM users";
-		$stmt = $this->conn->prepare($sql);
-		$stmt->execute();
-		$result = $stmt->fetchAll();
-		$reponse["status"] = false;
-		$reponse["message"] = "Get all users failed";
-		if (count($result) > 0) {
-			$reponse["status"] = true;
-			$reponse["message"] = "Get all users success";
-			$reponse["data"] = $result;
-		}
-		return $reponse;
+
 		$result = $this->prepareSQL($sql);
+		return $result;
 		$questions = $this->toJSON($result);
+
 		if (count($data) > 0) {
 			$questions = $this->filter_questions($questions, $data);
 		}
+
 		$response['status'] = false;
 		$response['message'] = "Get all questions failed";
 

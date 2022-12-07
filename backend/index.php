4a19111f44e581deae47120269000f88981c360d
BhryanS2 <bhryanstepenhen@gmail.com>
2022-12-07 09:04:41 -0300

    commit message

--- backend/index.php
+++ backend/index.php
@@ -24,7 +24,7 @@ $routes = [
 	],
 	"aluno" => [
 		"POST" => "RegisterAlunoController",
-		"DELETE" => "DeleteAlunoController"
+		"DELETE" => "DeleteAlunoController",
 	],
 	"blocos" => [
 		"GET" => "GetAllBlocosController"
---
+++ git.ps1
@@ -0,0 +1,8 @@
+# submit to github
+# git add .
+# git commit -m "commit message"
+# git push origin master
+
+git add .
+git commit -m "commit message"
+git push origin master

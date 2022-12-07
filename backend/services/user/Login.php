8129d351586cf7dfef84913be19513c691337eca
BhryanS2 <bhryanstepenhen@gmail.com>
2022-12-07 07:13:19 -0300

    login

--- backend/services/user/Login.php
+++ backend/services/user/Login.php
@@ -22,30 +22,31 @@ class LoginService
 		$sql = "SELECT * FROM users WHERE email = ?";
 		$stmt = $this->conn->prepare($sql);
 		$stmt->bind_param("s", $email);
-		$stmt->execute();
-		$result = $stmt->get_result();
-		$result = $result->fetch_all(MYSQLI_ASSOC);
-		if (count($result) > 0) {
-			$user = $result[0];
-			$userPassword = $user["password"];
-			if ($this->verifyPassword($userPassword, $password)) {
-				$reponse["status"] = true;
-				$reponse["message"] = "Login success";
-				$userNoPassword = [
-					"id" => $user["id"],
-					"name" => $user["name"],
-					"email" => $user["email"],
-					"role" => $user["role"],
-				];
-				$reponse["data"] = $userNoPassword;
-				return $reponse;
-			}
-			$response["status"] = false;
-			$response["message"] = "Login failed";
-			return $response;
-		}
-		$reponse["status"] = false;
-		$reponse["message"] = "Login failed";
+		$response['sql'] = $sql;
+		// $stmt->execute();
+		// $result = $stmt->get_result();
+		// $result = $result->fetch_all(MYSQLI_ASSOC);
+		// if (count($result) > 0) {
+		// 	$user = $result[0];
+		// 	$userPassword = $user["password"];
+		// 	if ($this->verifyPassword($userPassword, $password)) {
+		// 		$reponse["status"] = true;
+		// 		$reponse["message"] = "Login success";
+		// 		$userNoPassword = [
+		// 			"id" => $user["id"],
+		// 			"name" => $user["name"],
+		// 			"email" => $user["email"],
+		// 			"role" => $user["role"],
+		// 		];
+		// 		$reponse["data"] = $userNoPassword;
+		// 		return $reponse;
+		// 	}
+		// 	$response["status"] = false;
+		// 	$response["message"] = "Login failed";
+		// 	return $response;
+		// }
+		// $reponse["status"] = false;
+		// $reponse["message"] = "Login failed";
 		return $reponse;
 	}
 }
